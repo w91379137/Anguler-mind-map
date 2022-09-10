cb93db4ee6669394e93a6c89c7d95c47f05dfa3e
w91379137 <w913791379137@gmail.com>
2022-09-11 00:23:26 +0800

    [feat] 新增基本資料

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getItems, HomeViewModel } from './home.viewmodel';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  viewModel = new HomeViewModel()
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
+  constructor() {
+    this.viewModel.itemList = getItems()
+  }
 
   ngOnInit(): void {
+    
   }
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
 
 }
