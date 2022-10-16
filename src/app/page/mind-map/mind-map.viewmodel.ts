8dc77951a38f7f20747fd13e1a8164d5773eef30
w91379137 <w913791379137@gmail.com>
2022-10-17 01:46:05 +0800

    [feat] 基礎展開畫面

--- src/app/page/mind-map/mind-map.viewmodel.ts
+++ src/app/page/mind-map/mind-map.viewmodel.ts
@@ -1,18 +1,18 @@
 import { MindMapItem } from "src/app/service/frontend-anguler/mind-map-item";
 
-const ItemGap = 300
+const ItemGap = 150
 export class MindMapItemViewModel {
 
   // 有一個 item
 
-  childCount = 0
+  childCount = 0 // 有幾個小孩
 
   // 本身位置
   // x = Math.random() * 1000
   // y = Math.random() * 1000
   x = 0
   y = 0
-  childIndex = 0
+  childIndex = 0 // 他是第幾個小孩
 
   // 本身可分配方向 (防止child 相撞)
   start_theta = 0
@@ -42,20 +42,42 @@ export class MindMapViewModel {
   // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
   updatePosition(root_id: number) {
 
-    let child_id: number[] = [root_id]
     let root = this.itemVMList.find(itemVM => itemVM.id === root_id)
-    root.x = 500
-    root.y = 500
+    root.x = 300
+    root.y = 300
 
-    let checkList = this.itemVMList.slice(0) // 剩下的
-    for (let index = 0; index < 6; index++) {
+    // 讓大家找好 有幾個小孩
+    this.itemVMList.forEach(child => {
+      let parent = this.itemVMList.find(itemVM => itemVM.id === child.item.parentId)
+      if (parent) {
+        child.childIndex = parent.childCount
+        parent.childCount += 1
+      }
+    })
 
-      // 讓大家找好 有幾個小孩
-      
-      // 分配小孩位置
+    // 分配角度
+    // 依照順序
+    // 依照小孩個數x
+    // 目前都是照順序 之後再看看 如果沒有照順序 會怎樣
+    this.itemVMList.forEach(child => {
+      let parent = this.itemVMList.find(itemVM => itemVM.id === child.item.parentId)
+      if (parent) {
+        let range = parent.end_thata - parent.start_theta
+        let unit = range / parent.childCount
+        child.start_theta = parent.start_theta + child.childIndex * unit
+        child.end_thata = parent.start_theta + (child.childIndex + 1) * unit
+      }
+    })
 
-    }
+    // 分配小孩位置
+    this.itemVMList.forEach(child => {
 
+      let parent = this.itemVMList.find(itemVM => itemVM.id === child.item.parentId)
+      if (parent) {
+        child.x = parent.x + Math.cos(Math.PI / 180 * child.start_theta) * ItemGap
+        child.y = parent.y + Math.sin(Math.PI / 180 * child.start_theta) * ItemGap
+      }
+    })
 
     // 集體 位移
 
